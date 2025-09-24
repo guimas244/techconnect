816503499d0b44f56faeae0be429e1689e6ea80d
Antonio Carlos Guimarães de Faria Júnior <antonio.guimaraes@basisti.com.br>
2025-09-24 02:25:55 +0100

    feat: Update version to 1.5.0 in version_config and pubspec.yaml

--- lib/core/config/version_config.dart
+++ lib/core/config/version_config.dart
@@ -1,5 +1,5 @@
 class VersionConfig {
-  static const String currentVersion = '1.4.0';
+  static const String currentVersion = '1.5.0';
   
   /// Compara duas versões e retorna:
   /// 1 se version1 > version2
